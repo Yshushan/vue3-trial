5470a7916af2bb0f9cf31a32361c865c56ddced5
yangss3 <yss_2016@outlook.com>
2020-09-06 13:38:35 +0800

    update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>{{ msg }}</h1>
+    <h2>{{ new Date().toLocaleString() }}</h2>
+    <h2>{{ Math.max(1, 23, 4) }}</h2>
     <button @click="reverse">reverse</button>
   </div>
 </template>
